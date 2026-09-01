79fb3e73dd875c5606fc6f3954c382641a15ed7a
thegrateminecra <thegrateminecra@users.noreply.github.com>
2026-09-01 11:57:26 +0100

    fix: wrap module sections in do...end to fix out of local registers error

--- games/universal.lua
+++ games/universal.lua
@@ -935,6 +935,7 @@ run(function()
 	end)
 end)
 entitylib.start()
+do
 local AntiFall
 local Method
 local Mode
@@ -3694,9 +3695,11 @@ Distance = TriggerBot:CreateSlider({
 	Max = 1000,
 	Default = 1000,
 	Suffix = function(val)
-		return val == 1 and 'stud' or 'studs'
+	return val == 1 and 'stud' or 'studs'
 	end
 })
+end
+do
 local Atmosphere
 local Toggles = {}
 local newobjects, oldobjects = {}, {}
@@ -3843,6 +3846,8 @@ for i, v in apidump do
 		end
 	end
 end
+end
+do
 local Breadcrumbs
 local Texture
 local Lifetime
@@ -4792,10 +4797,12 @@ Value = TimeChanger:CreateSlider({
 	Default = 12,
 	Function = function(val)
 		if TimeChanger.Enabled then
-			lightingService.TimeOfDay = val..':00:00'
+	lightingService.TimeOfDay = val..':00:00'
 		end
 	end
 })
+end
+do
 
 local Arrows
 local Targets
@@ -6541,10 +6548,12 @@ Texture = PlayerModel:CreateTextBox({
 	Placeholder = 'texture id',
 	Function = function()
 		for _, part in models do 
-			part.Mesh.TextureId = Texture.Value
+		part.Mesh.TextureId = Texture.Value
 		end
 	end
 })
+end
+do
 
 local Radar
 local Targets
@@ -7597,9 +7606,11 @@ do
 					fill.Size = UDim2.fromScale(self.playPosition / self.playDuration, 1)
 				end
 			end
-		until false
+	until false
 	end
 end
+end
+do
 local Tracers
 local Targets
 local Color
@@ -7901,6 +7912,8 @@ Background = Waypoints:CreateSlider({
 	Max = 1,
 	Decimal = 10
 })
+end
+do
 
 local AnimationPlayer
 local IDBox
@@ -8953,3 +8966,4 @@ List = Xray:CreateTextList({
 		end
 	end
 })
+end
